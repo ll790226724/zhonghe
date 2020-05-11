1bb983372d47d0cdc817e2eca3c4821fd5a4253f
renhaihuang <790226724@qq.com>
2020-05-11 16:02:25 +0800

    feat: add digital resource

--- crane/components/talent_number_digital.js
+++ crane/components/talent_number_digital.js
@@ -9,9 +9,9 @@ module.exports = {
   },
 
   props: {
-    $url: "`/v1/components/89b74ddd-39de-493f-84ab-9d87fcf23fee/data?start=${craneStates.filterRange[0]}&end=${craneStates.filterRange[1]}`",
-    method: 'get',
-    $data: "[[0]]",
+    // $url: "`/v1/components/89b74ddd-39de-493f-84ab-9d87fcf23fee/data?start=${craneStates.filterRange[0]}&end=${craneStates.filterRange[1]}`",
+    // method: 'get',
+    // $data: "[[0]]",
     $style: {
       width: '194px',
       height: '44px',
@@ -26,7 +26,7 @@ module.exports = {
         titlePosition: 'left',
         $content: {
           title: '地区生产总值',
-          $digital: 'results[0][0]',
+          $digital: 23,
           suffix: '人',
         },
         $options: {
