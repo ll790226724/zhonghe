a00f3c2dfd363ad9852a52f63d7b0bbc6814c238
renhaihuang <790226724@qq.com>
2020-05-11 15:27:36 +0800

    feat: add graohite and carbonium in crane.config

--- crane.config.js
+++ crane.config.js
@@ -16,6 +16,18 @@ module.exports = {
         path: 'dist/index.css',
       },
     },
+
+    '@byzanteam/graphite': {
+      version: 'latest',
+    },
+
+    '@byzanteam/carbonium': {
+      version: 'latest',
+      style: {
+        type: 'normal',
+        path: 'dist/carbonium.css',
+      },
+    },
   },
   // 页面
   pages: [
