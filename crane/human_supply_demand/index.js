4eebadb469792db302536ca5047ddfd97c89d0d4
idcjaja <838927992@qq.com>
2020-05-28 16:06:38 +0800

    feat: add bg

--- crane/human_supply_demand/index.js
+++ crane/human_supply_demand/index.js
@@ -25,6 +25,53 @@ module.exports = {
     }
   ],
   components: [
+    {
+      id: 'background',
+      component: 'img',
+      position: [0, 0],
+      props: {
+        src: '/static/images/Bg.png'
+      },
+    },
+    {
+      id: 'title-bg',
+      component: 'img',
+      position: [607, 0],
+      props: {
+        $style: {
+          width: '701px',
+          height: '123px',
+        },
+        src: '/static/images/Title-Bg.png'
+      },
+    },
+    {
+      id: 'page-title',
+      component: 'div',
+      content: '人才供需专题',
+      position: [607, 27],
+      props: {
+        $style: {
+          width: '701px',
+          color: '#fff',
+          fontSize: '42px',
+          $fontWeight: '600',
+          textAlign: 'center',
+        }
+      }
+    },
+    {
+      id: 'box-bg',
+      component: 'img',
+      position: [10, 10],
+      props: {
+        $style: {
+          width: '440px',
+          height: '1059px',
+        },
+        src: '/static/images/Box-Bg.png'
+      },
+    },
     jobSelect
   ]
 }
