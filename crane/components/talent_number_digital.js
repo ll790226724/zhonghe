3261c4270fed3327b7c96a8fd8aff40dd260235d
idcjaja <838927992@qq.com>
2020-06-15 10:21:46 +0800

    feat: digitals data

--- crane/components/talent_number_digital.js
+++ crane/components/talent_number_digital.js
@@ -9,20 +9,20 @@ module.exports = {
   },
 
   props: {
-    // $url: "`/v1/components/89b74ddd-39de-493f-84ab-9d87fcf23fee/data?start=${craneStates.filterRange[0]}&end=${craneStates.filterRange[1]}`",
-    // method: 'get',
-    // $data: "[[0]]",
+    $url: "`/v1/components/13b74ddd-39de-493f-84ab-9d87fcf23fee/data`",
+    method: 'get',
+    $data: "[[0]]",
   },
   children: [
     {
       id: 'talent-number-content',
       component: '@byzanteam/vis-components/digital-roll',
       props: {
-        // 'v-if': 'results',
+        'v-if': 'results',
         titlePosition: 'left',
         $content: {
           title: '中高端人才数量',
-          $digital: 5200,
+          $digital: "results[0][0]",
           suffix: '人',
         },
         $options: {
