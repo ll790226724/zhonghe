b9e083d4207176fab746011bf00aae138daf677b
renhaihuang <790226724@qq.com>
2020-08-20 10:52:38 +0800

    fix: 雷达图默认福州厦门市

--- src/views/talents_competitiveness.vue
+++ src/views/talents_competitiveness.vue
@@ -172,7 +172,7 @@ export const talents_competitiveness = {
         indicators: [{name: '人才数量指标'}, {name: '人才质量指标'}, {name: '人才结构指标'}, {name: '人才投入指标'}, {name: '人才平台指标'}, {name: '人才生活指标'}, {name: '人才环境指标'}, {name: '人才效能指标'}, {name: '人才效益指标'}, {name: '人才发展指标'}],
         indicator: '人才数量指标',
         types: [{index: 1, name: '四川省'}, {index: 2, name: '重庆市'}, {index: 3, name: '青海省'}, {index: 4, name: '浙江省'}, {index: 5, name: '湖南省'}, {index: 6, name: '湖北省'}, {index: 7, name: '甘肃省'}, {index: 8, name: '山东省'}, {index: 9, name: '江苏省'}, {index: 10, name: '江西省'}, {index: 11, name: '福建省'}, {index: 12, name: '贵州省'}, {index: 13, name: '陕西省'}, {index: 14, name: '山西省'}],
-        currentProvince: [],
+        currentProvince: ['厦门市', '福州市'],
         selectedArea: {},
         radarData: [],
         mapData: [],
